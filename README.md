ba00d0a401dfe2438d7c1c301c099ac49af70837
nmougino <nmougino@student.42.fr>
2016-08-30 19:16:33 +0200

    avancement recherches

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ls
 Projet pedagogique
+
+Reflexion sur la methode:
+
+liste doublement chainee (et circulaire?) pour traiter le tri facilement.
+Attention, la gestion en liste chainee impliquerait des millions de mallocs :O
+
+En recursif cela va considerablement augmenter la necessite d'espace memoire.
+
+La liste chainee reste la meilleure possibilite de stockage des donnees en cache.
+
+
+Update:
+Utiliser lstat afin que dans le cas d'un lien symbolique, on extrait les donnes du lien
+et pas du fichier qu'il pointe.
--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/28 19:32:58 by nmougino          #+#    #+#             */
-/*   Updated: 2016/08/28 20:27:39 by nmougino         ###   ########.fr       */
+/*   Updated: 2016/08/29 13:07:51 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,22 @@
 # include <sys/types.h>
 # include <time.h>
 
-typedef struct	dirent t_dirent;
+typedef struct	dirent	t_dirent;
 
-void	ft_inspect_file(const char *path);
-int		ft_list_dir(const char *filename);
+void					ft_inspect_file(const char *path);
+int						ft_list_dir(const char *filename);
+
+typedef struct			s_meta
+{
+	char				param;
+	int					(*sort_fun)(t_contenu *elem, t_contenu *fst);
+	t_contenu			*fst;
+}						t_meta;
+
+typedef struct			s_contenu
+{
+	struct s_contenu	*next;
+	struct stat			st_data;
+}						t_contenu;
 
 #endif

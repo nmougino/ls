1fa06a987473a622b48b18543a55acd25dbd61ae
nmougino <nmougino@student.42.fr>
2016-09-07 22:04:01 +0200

    Modif display behaves like -1 option

--- srcs/display.c
+++ srcs/display.c
@@ -6,7 +6,7 @@
 /*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/01 23:36:13 by nmougino          #+#    #+#             */
-/*   Updated: 2016/09/06 07:10:49 by nmougino         ###   ########.fr       */
+/*   Updated: 2016/09/07 21:54:52 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,9 @@
 
 void	display(t_ls_file *file)
 {
-	register int	col;
-	register int	len;
-
-	col = com_name(file);
 	while (file)
 	{
-		len = ft_strlen(file->name);
-		write(1, file->name, len);
-		if (!(file->next))
-			write(1, "\n", 1);
-		else
-			ft_printf("%*c", col - len, ' ');
+		ft_putendl(file->dp.d_name);
 		file = file->next;
 	}
 }

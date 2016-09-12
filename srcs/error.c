0f6cf33dace0e40b53991d5a4bf14e5887d6a87c
nmougino <nmougino@student.42.fr>
2016-09-12 23:00:10 +0200

    Fix stderr output

--- srcs/error.c
+++ srcs/error.c
@@ -6,7 +6,7 @@
 /*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/31 02:02:37 by nmougino          #+#    #+#             */
-/*   Updated: 2016/09/11 22:01:12 by nmougino         ###   ########.fr       */
+/*   Updated: 2016/09/12 22:56:30 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 int		error_w_param(char c)
 {
-	ft_printf("ft_ls: illegal option -- %c\nusage: ft_ls [-Ralnrt] [file ...]\n"
-		, c);
+	ft_dprintf(2, "ft_ls: illegal option -- %c\n%s\n", c,
+		"usage: ft_ls [-FRUacflnprtu] [file ...]");
 	return (-1);
 }
 
 void	arg_error(char *name)
 {
-	ft_printf("ft_ls: %s: ", name);
+	ft_dprintf(2, "ft_ls: %s: ", name);
 	perror(NULL);
 }
